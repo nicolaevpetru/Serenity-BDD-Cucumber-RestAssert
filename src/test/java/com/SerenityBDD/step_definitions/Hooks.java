a293037f8e7c2d07a3b184adfe76bf0b6de43d87
Petru <nicolaevpetru@outlook.com>
2021-02-09 22:44:47 -0500

    ScreenRecorderUtils added

--- src/test/java/com/SerenityBDD/step_definitions/Hooks.java
+++ src/test/java/com/SerenityBDD/step_definitions/Hooks.java
@@ -1,6 +1,7 @@
 package com.SerenityBDD.step_definitions;
 
 import com.SerenityBDD.utils.Environment;
+import com.SerenityBDD.utils.ScreenRecorderUtils;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
@@ -12,13 +13,14 @@ public class Hooks {
     private final static Logger log = LogManager.getLogger(Hooks.class);
 
     @Before
-    public void setUp(Scenario scenario) {
+    public void setUp(Scenario scenario) throws Exception {
         scenario.log("#### TESTING IN [ " + Environment.getEnvironment() + " ] ENVIRONMENT.");
         RestAssured.baseURI = Environment.getProperty("baseURI");
+//        ScreenRecorderUtils.startRecord("Regression");
     }
 
     @After
-    public void tearDown(Scenario scenario) {
+    public void tearDown(Scenario scenario) throws Exception {
         if (scenario.isFailed()) {
             scenario.log(scenario.getName() + "####### [ TEST FAILED ].");
             log.info(scenario);
@@ -26,6 +28,7 @@ public class Hooks {
             scenario.log("\n #######  [ TEST PASS ] \n");
 //            scenario.log(scenario.getName());
         }
+//        ScreenRecorderUtils.stopRecord();
         RestAssured.reset();
     }
 }
